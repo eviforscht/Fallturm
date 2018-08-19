49d942ee48d7e9467ddaa25b15b6e458a5e220ad
AnonymOnline <david@anonym-online.net>
2018-08-19 13:21:51 +0200

    add appveyor support

---
+++ Desktop/Fallturm/logger.cpp
@@ -0,0 +1,164 @@
+#include "logger.h"
+
+
+
+
+//Static variables
+Logger Logger::log = Logger();
+
+/**
+* Initializing the logger
+*
+* @brief QTLogger::init
+*/
+
+
+
+void Logger::init() {
+
+    QDateTime Time;
+    QString FileName;
+    QString SaveFileName;
+    QString FileVersion;
+
+    FileVersion = Time.currentDateTime().toString("yyyy-MM-dd");
+    FileVersion.replace(":", "_");
+    FileVersion.replace("-", "_");
+    FileVersion.replace(" ", "-");
+
+    FileName = QString("log.txt");
+    SaveFileName = QString("log_%1.txt").arg(FileVersion);
+
+    outFile = new QFile(FileName);
+    outSaveFile = new QFile(SaveFileName);
+
+    outFile->open(QIODevice::WriteOnly);
+    outSaveFile->open(QIODevice::WriteOnly);
+    ts = new QTextStream(outFile);
+    sts = new QTextStream(outSaveFile);
+
+    debug = true;
+    info = true;
+    warning = true;
+    error = true;
+
+
+}
+
+void Logger::init(int LoggerLevel) {
+
+    Logger::init();
+
+    debug = false;
+    info = false;
+    warning = false;
+    error = false;
+
+    if (LoggerLevel & LoggerOutput::Debug)
+    {
+        debug = true;
+    }
+
+    if (LoggerLevel & LoggerOutput::Info)
+    {
+        info = true;
+    }
+
+    if (LoggerLevel & LoggerOutput::Warn)
+    {
+        warning = true;
+    }
+
+    if (LoggerLevel & LoggerOutput::Error)
+    {
+        error = true;
+    }
+
+}
+
+
+Logger &operator << (Logger &l, const char * a)
+{
+    QString str = QString::fromUtf8(a);
+
+    l.send(str);
+
+    return l;
+}
+Logger &operator << (Logger &l, QString a)
+{
+    l.send(a);
+
+    return l;
+}
+Logger &operator << (Logger &l, string a)
+{
+    QString str;
+
+    str = QString::fromStdString(a);
+
+
+    l.send(str);
+
+    return l;
+}
+Logger &operator << (Logger &l, int a)
+{
+
+    QString str;
+
+    str.sprintf("%d", a);
+
+    l.send(str);
+
+    return l;
+}
+
+void Logger::send(QString str)
+{
+    QString LogTime;
+
+    if ((str == L_INFO && info == true) || (str == L_ERROR && error == true) || (str == L_WARN && warning == true) || (str == L_DEBUG && debug == true))
+    {
+        Logger::sending_permission = true;
+    }
+
+    Logger::value.append(str);
+    int endcounter = Logger::value.count("\n");
+    if (endcounter > 0)
+    {
+
+        Logger::value.remove("\n");
+
+
+        QDateTime time;
+        LogTime.append(time.currentDateTime().toString("hh:mm:ss:zzz"));
+        LogTime.append("  ");
+
+        LogTime.append(value);
+
+        if (sending_permission == true)
+        {
+            qDebug().noquote() << LogTime;
+
+
+            *Logger::ts << LogTime << "\n";
+            Logger::ts->flush();
+            *Logger::sts << LogTime << "\n";
+            Logger::sts->flush();
+            Logger::sending_permission = 0;
+        }
+        Logger::value.clear();
+    }
+
+}
+
+
+Logger::~Logger()
+{
+    delete ts;
+    delete sts;
+    delete outFile;
+    delete outSaveFile;
+}
+
---
+++ Desktop/Fallturm/logger.h
@@ -0,0 +1,83 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+//#include <Qdialog>
+#include <QDebug>
+#include <QFile>
+//#include <QErrorMessage>
+#include <QString>
+//#include <iostream>
+#include <sstream>
+#include <string>
+#include <QTime>
+#include <QTimer>
+
+
+using namespace std;
+
+
+#define L_ERROR "ERROR    "
+#define L_WARN  "WARNING  "
+#define L_INFO  "INFO     "
+#define L_DEBUG "DEBUG    "
+
+
+/**
+ * Logger based on log4cpp
+ *
+ * @brief The Logger class
+ */
+class Logger
+{
+public:
+    void init();
+    void init(int LoggerLevel);
+    static Logger       log;
+
+    enum LoggerOutput{None  = 0,
+                      Error  = 8,
+                      Warn   = 4,
+                      Info   = 2,
+                      Debug  = 1,
+                      All    = 15,
+                      Level1 = LoggerOutput::Error,
+                      Level2 = LoggerOutput::Error | LoggerOutput::Warn,
+                      Level3 = LoggerOutput::Error | LoggerOutput::Warn | LoggerOutput::Info,
+                      Level4 = LoggerOutput::All
+                     };
+
+private:
+    void         send(QString str);
+    QString      value;
+    QFile        *outFile;
+    QFile        *outSaveFile;
+    QTextStream  *ts;
+    QTextStream  *sts;
+    bool         sending_permission;
+    bool         error;
+    bool         warning;
+    bool         info;
+    bool         debug;
+
+    friend Logger &operator << (Logger& l, const char * a);
+    friend Logger &operator << (Logger& l, string a);
+    friend Logger &operator << (Logger& l, QString a);
+    friend Logger &operator << (Logger& l, int a);
+
+    //None	= keine Ausgabe
+    //Error	= nur Error
+    //Warn	= nur Warning
+    //Info	= nur Info
+    //Debug	= nur Debug
+    //All		= alle werden ausgegeben
+    //Level1	= nur Error
+    //Level2	= Error und Warning,
+    //Level3	= Error, Warning und Info,
+    //Level4	= alle werden ausgegeben
+
+
+    Logger() = default;
+    ~Logger();
+};
+
+#endif // QT_LOGGER_H
